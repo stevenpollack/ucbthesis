6132816274d9854f667f1d2dd1248e5c3fe193ef
Steven Pollack <steven@pollackphoto.net>
2014-05-22 17:31:31 -0500

    updated DESCRIPTION and NAMESPACE.

--- R/ucbthesis-package.r
+++ R/ucbthesis-package.r
@@ -1,4 +1,7 @@
-#' ucbthesis.
+#' @title UC Berkeley Graduate Division Thesis Templates
+#' @description This package contains latex, knitr and R Markdown templates that
+#' adhere to the UC Berkeley Graduate Division's thesis guidelines. The templates
+#' are located in the \code{inst/} directory.
 #'
 #' @name ucbthesis
 #' @docType package
